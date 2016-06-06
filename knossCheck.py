95757b7e0487e247732bf7784dce60ce167315a3
nathantspencer <nathantspencer@gmail.com>
2016-06-06 12:11:25 -0400

    checks for pre-existing nrrds

--- knossCheck.py
+++ knossCheck.py
@@ -9,6 +9,7 @@ zips = []
 dropbox_path = '/home/curie/knoss2obj/Webknossos/'
 data_path = '/home/curie/knoss2obj/data/'
 obj_path = '/home/curie/knoss2obj/obj/'
+nrrd_path = '/home/curie/knoss2obj/obj/'
 print('Starting matlab engine...')
 eng = matlab.engine.start_matlab()
 
@@ -22,18 +23,19 @@ for root, dirnames, filenames in os.walk(dropbox_path, followlinks=True):
 # Extract raws from each zip and convert to obj
 names = []
 for i in range(len(zips)):
-    print('Converting raw set to obj: {} of {}'.format(i+1, len(zips)))
     raws_to_delete = glob.glob('data/*.raw')
     for raw in raws_to_delete:
         os.remove(raw)
     names.append(zips[i].split('/')[len(zips[i].split('/'))-1][:-4])
+    print('Converting {}...  {} of {}'.format(names[i], i+1, len(zips)))
 
-    # Skip raw set if corresponding obj already exists and is finished
+    # Skip raw set if corresponding obj/nrrd already exists
     current_obj_path = obj_path + '/' + names[i] + '.obj'
-    current_obj_name = 'obj\\' + names[i] + '.obj'
+    current_obj_name = 'obj/' + names[i] + '.obj'
+    current_nrrd_path = nrrd_path + '/' + names[i] + '.nrrd'
+    current_nrrd_name = 'nrrd/' + names[i] + '.nrrd'
 
-    # Broken
-    if current_obj_name in glob.glob('obj\*.obj') and os.path.getmtime(zips[i]) > os.path.getmtime(current_obj_path):
+    if current_obj_name in glob.glob('obj/*.obj') and current_nrrd_name in glob.glob('nrrd/*.nrrd'):
         print('Raw set {} is already up to date.'.format(i+1))
         continue
     current_zip = zf.ZipFile(zips[i])
