98cdf95dc411572ec1785156d25aa2f6872e88e8
nathantspencer <nathantspencer@gmail.com>
2016-06-21 11:24:29 -0400

    fixed path bug that had broken everything

--- startObj.m
+++ startObj.m
@@ -1,7 +1,7 @@
 function success =  start( out_name ) 
 
 path = [pwd,'/',mfilename];
-path = path(1:length(path)-6);
+path = path(1:length(path)-10);
 addpath(genpath(path));
 path = strcat(path, '/data/');
 
